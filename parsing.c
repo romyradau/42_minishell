c87f3b19e9f48d229764460b4614d6ca86f6b6b6
rschleic <rschleic@student.42heilbronn.de>
2022-02-10 19:02:56 +0100

    all uptodate

--- parsing.c
+++ parsing.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mjeyavat <mjeyavat@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rschleic <rschleic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 17:08:59 by mjeyavat          #+#    #+#             */
-/*   Updated: 2022/02/10 18:51:06 by mjeyavat         ###   ########.fr       */
+/*   Updated: 2022/02/10 19:01:53 by rschleic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,3 +32,31 @@ void init_pars(char **tokens)
         i++;
     }
 }
+
+/*
+shell gets input
+ignores it if # in the beginning
+devides input in words and operators
+employs quoting rules on the words and characters
+*/
+
+/*
+Breaks the input into words and operators, obeying the quoting rules
+These tokens are separated by metacharacters
+quoting rules:
+1.quotes - double -single
+1.2 env Variables
+2.herdoc
+3.redirections
+4.pipes
+5.cmds -args
+6.eigene commands
+extra stuff 
+$?
+anfuhrungszeichen und spaces entfernen bei weirden langen commands
+*/
+
+
+/*
+parses tokens in commands
+*/
