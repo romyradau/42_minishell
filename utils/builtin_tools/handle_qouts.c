12045a2993995c26b4a99675b5ffb94cc9905a5a
Moghanavathanan Jeyavathanan <mjeyavat@1-E-7.42heilbronn.de>
2022-02-25 14:32:05 +0100

    builtins.c is now seperate, every thing made more redable

---
+++ utils/builtin_tools/handle_qouts.c
@@ -0,0 +1,69 @@
+#include "../../minishell.h"
+
+
+int check_quot_sequence(char *str, char c, bool *q)
+{
+    int i;
+
+    i = 0;
+    if (str[i] == c)
+    {
+		(*q) = true;
+        i = ft_strlen(str)-1;
+        if (str[i] == c)
+            return (1);
+    }
+    return (0);
+}
+
+char    *cut_quot_sequence(char *str, char c)
+{
+    int     i;
+	bool 	quots;
+    char    *new_str;
+
+    i = 1;
+	quots = false;
+    new_str = (char *)malloc(ft_strlen(str) * sizeof(char));
+    if (str)
+    {
+        if (check_quot_sequence(str, c , &quots))
+            new_str = ft_substr(str,1, ft_strlen(str) - 2);
+        return (new_str);
+    }
+    else
+        return (NULL);
+}
+
+char *handle_qouts(char **cmd_arg, int index)
+{
+	bool	quots;
+	char	*output;
+
+	output = NULL;	
+	quots = false;
+	if (check_quot_sequence(cmd_arg[index], '"', &quots) || check_quot_sequence(cmd_arg[index], 39, &quots))
+	{
+		if (check_quot_sequence(cmd_arg[index], 39, &quots))
+			output = cut_quot_sequence(cmd_arg[index], 39);
+		else if (check_quot_sequence(cmd_arg[index], '"', &quots))
+			output = cut_quot_sequence(cmd_arg[index], '"');
+		quots = false;
+	}
+	else if (!check_quot_sequence(cmd_arg[index], '"', &quots) || check_quot_sequence(cmd_arg[index], 39, &quots))
+	{
+		if (quots)
+		{
+			printf("error!\n");
+			return (NULL);
+		}
+		else
+		{
+			printf("\nin handle_qouts : the string is allocated\n");
+			output = ft_strdup(cmd_arg[index]);
+			printf(".....\nAllocation is finished!\n");
+		}
+
+	}
+	return (output);
+}
---
+++ utils/builtin_tools/handle_string.c
@@ -0,0 +1,57 @@
+#include "../../minishell.h"
+
+char *ft_strcalloc(int size)
+{
+	char *new_s;
+	int i;
+
+	new_s = malloc(size);
+	if (!new_s)
+		return (NULL);
+	i = 0;
+	while (i < size)
+		new_s[i++] = 0;
+	return (new_s);
+}
+
+int doublestr_len(char **cmd_arg)
+{
+	int i;
+	int j;
+	int s_t;
+
+	i = 0;
+	j = 0;
+	s_t = 0;
+	while (cmd_arg[i])
+	{
+		while (cmd_arg[i][j])
+			j++;
+		s_t += j;
+		i++;
+	}
+	return (s_t);
+}
+
+char **kill_d_str(char **str)
+{
+	char **tmp;
+	int len;
+	int i;
+
+	tmp = str;
+	len = 0;
+	i = 0;
+	while (*tmp)
+	{
+		tmp++;
+		len++;
+	}
+	while (i < len)
+	{
+		str[i] = ft_strcalloc(1);
+		i++;	
+	}
+	free(str);
+	return (str);
+}
